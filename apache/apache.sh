2801c034ff9b119c16ffc235a0b87e2454415fe7
pogforgor <143901218+pogforgor@users.noreply.github.com>
2024-06-16 22:29:38 +0100

    Create apache.sh

---
+++ apache/apache.sh
@@ -0,0 +1,7 @@
+# Instalar apache
+sudo apt update
+sudo apt install apache2
+
+# Substituir index.html original por outro
+# Fonte: https://ubuntu.com/tutorials/install-and-configure-apache#3-creating-your-own-website
+sudo cp index.html /var/www/html/index.html
