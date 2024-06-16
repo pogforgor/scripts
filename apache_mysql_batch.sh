abbae02137312302f2dfc1ae114cf0e1b9403953
pogforgor <143901218+pogforgor@users.noreply.github.com>
2024-06-16 22:47:40 +0100

    Create apache_mysql_batch.sh

---
+++ apache_mysql_batch.sh
@@ -0,0 +1,14 @@
+# Dá permissões de execução a todos os scripts
+chmod 777 apache/*.sh
+chmod 777 mysql/*.sh
+
+# Apache - Instalação
+
+cd apache
+./apache.sh
+
+# MySQL - Instalação e instruções
+
+cd ..
+cd mysql
+./mysql_preinstaller.sh
