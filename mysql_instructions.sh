f71d49099b9f08a747e7fffa086a30f1fa08971e
pogforgor <143901218+pogforgor@users.noreply.github.com>
2024-06-16 21:54:09 +0100

    Create mysql_instructions.sh

---
+++ mysql_instructions.sh
@@ -0,0 +1,17 @@
+# IGNORA LINHAS QUE COMEÇAM COM #
+# Entra como root: (sem prompt de password):
+sudo mysql -u root -p
+
+# Cria a database "wordpress":
+CREATE DATABASE wordpress;
+
+# Cria um utilizador "wordpress":
+# NOTA: Cria uma palavra-passe e substitui o 'your_password' com ela!
+CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'your_password';
+
+# Permissões
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_user'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+
+# Fim
