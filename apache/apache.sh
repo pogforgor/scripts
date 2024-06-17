6fd88b3b3de2399051a527aa75e9658ab7c0e4e5
pogforgor <143901218+pogforgor@users.noreply.github.com>
2024-06-17 11:05:52 +0100

    Update apache.sh

--- apache/apache.sh
+++ apache/apache.sh
@@ -1,6 +1,6 @@
 # Instalar apache
 sudo apt update
-sudo apt install apache2
+sudo apt install apache2 -y
 
 # Substituir index.html original por outro
 # Fonte: https://ubuntu.com/tutorials/install-and-configure-apache#3-creating-your-own-website
